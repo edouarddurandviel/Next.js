80fcd5e67bc0757542dbff3d608bfe275e5d4994
Edouard <edurand.viel@gmail.com>
2026-03-10 17:46:10 +0100

    nextconfig image svg

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,15 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
    images: {
-    remotePatterns: [new URL('https://placehold.co/600x400/**')]
+    remotePatterns: [new URL('https://placehold.co/600x400/**')],
+    formats: ['image/avif', 'image/webp'],
+    minimumCacheTTL: 14400, // 4 hours
+    qualities: [25, 50, 75, 100],
+    maximumRedirects: 3,
+    dangerouslyAllowSVG: true,
+    contentDispositionType: 'attachment',
+    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
+    dangerouslyAllowLocalIP: true,
   },
 };
 
