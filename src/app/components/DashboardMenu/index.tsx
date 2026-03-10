4a5c277c2f80b8c130687839e671c211b4efa42b
Edouard <edurand.viel@gmail.com>
2026-03-10 16:28:15 +0100

    menu dashboard

--- src/app/components/DashboardMenu/index.tsx
+++ src/app/components/DashboardMenu/index.tsx
@@ -2,17 +2,14 @@
 import { usePathname } from "next/navigation";
 import { Aside, AsideMenu } from "./styles";
 import Link from "next/link";
+import { Dashboard } from "@app/routes/routes";
 
 const DashboardMenu = () => {
   const pathname = usePathname();
-  const routes = [
-    { label: "Analytics", url: "/dashboard/analytics" },
-    { label: "Settings", url: "/dashboard/settings" },
-  ];
   return (
     <Aside>
       <AsideMenu>
-        {routes.map((route, index) => (
+        {Dashboard.map((route, index) => (
           <Link
             key={index}
             href={route.url}
--- src/app/routes/routes.ts
+++ src/app/routes/routes.ts
@@ -3,7 +3,7 @@ export const HearMenu = [
   { label: "Blog", url: "/blog" },
   { label: "Logout", url: "/logout" },
   { label: "Login", url: "/login" },
-  { label: "Dashboard", url: "/dashboard" },
+  { label: "Dashboard", url: "/dashboard"},
   { label: "Newsletter", url: "/newsletter" },
   { label: "Abount", url: "/about" },
 ];
@@ -12,3 +12,8 @@ export const BlogMenu = [
   { label: "Blog", url: "/blog" },
   { label: "Article", url: "/blog/:slug" },
 ];
+
+ export const Dashboard = [
+    { label: "Agile", url: "/dashboard/analytics" },
+    { label: "Settings", url: "/dashboard/settings" },
+  ];
